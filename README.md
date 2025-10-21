8b5ead874f4f30b8915cc26a120ed5870e7c7aad
Charalampos Polychronakis <polychronakis.h@gmail.com>
2025-10-21 13:41:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Simple REST API with use of FastAPI in Python
+
+# Purpose
+Just a simple FastAPI application with purpose to learn the framework with REST API best practices
+
+## How to run the application
+The following two sections will give you the commands to run the application in your machine
+
+### Local Setup
+#### Setup Virtual Environment
+```
+python -m venv my_env
+```
+
+#### Install Requirements
+```
+pip install -r requirements.txt
+```
+
+#### Run Fast
+```
+fastapi run api_app.py
+```
+
+### Local Setup - Docker
---
+++ api_app.py
@@ -0,0 +1,52 @@
+# The tutorial which was used.
+# https://realpython.com/get-started-with-fastapi/#create-the-most-minimal-fastapi-app
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+books = [
+    { "id": 1, "title": "Python Basics", "author": "Real P.", "pages": 635 },
+    { "id": 2, "title": "Breaking the Rules", "author": "Stephen G.", "pages": 99 },
+    { "id": 3, "title": "Lord of the Rings", "author": "J.R.R.Tolkien", "pages": 700 }
+]
+
+class Book(BaseModel):
+    title: str
+    author: str
+    pages: int
+
+@app.get("/")
+def index():
+    print("First command in the same line!"); print("Second command in the same line!")
+    return { "message": "Hello World! Hello Fast API" }
+
+@app.get("/books")
+def get_books(limit: int | None = None):
+    """ Get all books, optionally limited by count. """
+    if limit:
+        return { "books": books[:limit] }
+    return { "books": books }
+
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    """ Get specific book by ID. """
+    for book in books:
+        if book["id"] == book_id:
+            return book
+    
+    return { "error": "Book not found" }
+
+
+@app.post("/books")
+def create_book(book: Book):
+    new_book = {
+        "id": len(books) + 1,
+        "title": book.title,
+        "author": book.author,
+        "pages": book.pages
+    }
+
+    books.append(new_book)
+    return new_book
